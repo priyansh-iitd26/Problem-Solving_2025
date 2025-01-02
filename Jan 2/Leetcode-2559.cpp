322a86c12dfa6f0474512db8d5bbdd0ae9677fee
Priyansh Kumar <162098447+priyansh-iitd26@users.noreply.github.com>
2025-01-02 12:17:07 +0530

    Update Leetcode-2559.cpp

--- Jan 2/Leetcode-2559.cpp
+++ Jan 2/Leetcode-2559.cpp
@@ -1,1 +1,71 @@
+// 2559. Count Vowel Strings in Ranges
+// You are given a 0-indexed array of strings words and a 2D array of integers queries.
+// Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
 
+// Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+// Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+// Example 1:
+
+// Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+// Output: [2,3,0]
+// Explanation: The strings starting and ending with a vowel are "aba", "ece", "aa" and "e".
+// The answer to the query [0,2] is 2 (strings "aba" and "ece").
+// to query [1,4] is 3 (strings "ece", "aa", "e").
+// to query [1,1] is 0.
+// We return [2,3,0].
+// Example 2:
+
+// Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+// Output: [3,2,1]
+// Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+//Constraints:
+// 1 <= words.length <= 105
+// 1 <= words[i].length <= 40
+// words[i] consists only of lowercase English letters.
+// sum(words[i].length) <= 3 * 105
+// 1 <= queries.length <= 105
+// 0 <= li <= ri < words.length
+
+// My Code :
+
+class Solution {
+private:
+    bool isVowel(char ch) {
+        ch = tolower(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        int n = words.size();
+        vector<int> arr(n, 0);
+
+        for(int i = 0; i < n; i++){
+            string word = words[i];
+
+            if(isVowel(word.front()) && isVowel(word.back())){
+                arr[i] = 1;
+            }
+        }
+
+        vector<int> prefixSum(n, 0);
+        prefixSum[0] = arr[0];
+
+        for(int i = 1; i < n; i++){
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        int q = queries.size();
+        vector<int> ans(q, 0);
+
+        for(int i = 0; i < q; i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            ans[i] = prefixSum[r] - (l > 0 ? prefixSum[l - 1] : 0);
+        }
+
+        return ans;
+    }
+};
